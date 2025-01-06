5da869d40c5e8763ec138c1084d4c72bb84b8995
Vladislav Klimenko <vlad991199@yandex.ru>
2025-01-06 16:05:25 +0300

    Echo chating works

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,17 +1,27 @@
 <!-- src/components/HelloWorld.vue -->
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>Welcome!</h1>
+    <div>
+      <p id="answer">{{ answer }}</p>
+      <input id="message" v-model="inputMessage" placeholder="Type a message" />
+      <button @click="sendMessage">Send message</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: {
-      type: String,
-      required: true
+  data() {
+    return {
+      answer: 'No answer yet',
+      inputMessage: ''
+    }
+  },
+  methods: {
+    sendMessage() {
+      this.answer = this.inputMessage
     }
   }
 }
@@ -20,5 +30,16 @@ export default {
 <style scoped>
 h1 {
   color: blue;
+}
+button {
+  margin-top: 10px;
+  padding: 5px 10px;
+  background-color: #4CAF50;
+  color: white;
+  border: none;
+  cursor: pointer;
+}
+button:hover {
+  background-color: #45a049;
 }
 </style>
--- frontend/src/components/tests/HelloWorld.spec.js
+++ frontend/src/components/tests/HelloWorld.spec.js
@@ -3,13 +3,24 @@ import { mount } from '@vue/test-utils'
 import HelloWorld from '../HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
-  it('renders the message passed as a prop', () => {
-    const msg = 'Hello2, Vue!'
-    const wrapper = mount(HelloWorld, {
-      props: { msg }
-    })
-
-    // Check that the message prop is rendered inside the <h1> tag
-    expect(wrapper.find('h1').text()).toBe(msg)
+  it('write text in input', async () => {
+    const wrapper = mount(HelloWorld)
+    const newMsg = 'It\'s me!'
+    const input = wrapper.find('#message')
+    await input.setValue(newMsg)  // Set the input value
+    const inputMsg = input.element.value  // Get the input value
+    expect(inputMsg).toBe(newMsg)
   })
+  it('check echo messaging', async () => {
+    const wrapper = mount(HelloWorld)
+    const input = wrapper.find('#message')  // Find the input field
+    const msg = 'Hello, Vue!'
+    await input.setValue(msg)  // Set the input value
+    const button = wrapper.find('button')  // Find the button
+    await button.trigger('click')  // Simulate the button click
+    let answer = wrapper.find('#answer').text()
+    expect(answer).toBe(msg)
+
+  })
+
 })
